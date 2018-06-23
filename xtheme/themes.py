3ae98758672afa5924fa02f385342ec7b6ef2f47
Adrian Cybulski <adrian@rtgnx.xyz>
2018-06-23 11:01:16 +0100

    Load config file listed in base.toml

--- xtheme/themes.py
+++ xtheme/themes.py
@@ -23,12 +23,21 @@ class Theme(object):
     if isfile(join(XTHEME_DIR, 'base.toml')):
       base = toml.loads(expandvars(fread(join(XTHEME_DIR, 'base.toml'))))
 
+    if 'config' in base.keys() and type(base['config']) == dict:
+      for k, v in base['config'].items():
+        path = join(join(XTHEME_DIR, "config"), v)
+        try:
+          cfg = toml.loads(expandvars(fread(path)))
+          base[k] = cfg
+        except:
+          continue
+
     t = fread(join(THEMES, name))
 
     if t is None:
       return t
 
-    t = toml.loads(t)
+    t = toml.loads(expandvars(t))
 
     if type(base) == dict:
       t = {**base, **t}
